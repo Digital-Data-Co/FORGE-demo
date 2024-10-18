98c1a65e341453bc211bfa2c5638d905a6588bd1
Chris Dixon <chriswdixon@gmail.com>
2024-10-18 16:31:12 -0500

    Added ASG

--- image/client/terraform/azure/DEV/infrastructure.tf
+++ image/client/terraform/azure/DEV/infrastructure.tf
@@ -74,6 +74,122 @@ resource "azurerm_container_registry" "acr" {
 }
 
 
+# Application Security Gateway with WAF v2 for SIPR connection via DoDIN
+resource "azurerm_public_ip" "main" {
+  name                = "scope-${var.environment}-main-pip"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
+
+# since these variables are re-used - a locals block makes this more maintainable
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.primary.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.primary.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.primary.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.primary.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.primary.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.primary.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.primary.name}-rdrcfg"
+}
+
+resource "azurerm_application_gateway" "network" {
+  name                = "main-appgateway"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.inside.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+
+}
+
+# Bastion
+
+#resource "azurerm_public_ip" "bastion" {
+#  name                = "scope-${var.environment}-pip"
+#  location            = data.azurerm_resource_group.main.location
+#  resource_group_name = data.azurerm_resource_group.main.name
+#  allocation_method   = "Static"
+#  sku                 = "Standard"
+#}
+
+#resource "azurerm_virtual_network" "bastion" {
+#  name                = "BastionVNet"
+#  address_space       = ["10.0.0.224/27"]
+#  location            = data.azurerm_resource_group.main.location
+#  resource_group_name = data.azurerm_resource_group.main.name
+#}
+
+#resource "azurerm_subnet" "bastion" {
+#  name                 = "AzureBastionSubnet"
+#  resource_group_name  = data.azurerm_resource_group.main.name
+#  virtual_network_name = azurerm_virtual_network.bastion.name
+#  address_prefixes     = ["10.0.0.224/27"]
+#}
+
+
+#resource "azurerm_bastion_host" "bastion" {
+#  name                = "scope-${var.environment}-bastion"
+#  location            = data.azurerm_resource_group.main.location
+#  resource_group_name = data.azurerm_resource_group.main.name
+#  tunneling_enabled = true
+#  sku = "Standard"
+  
+# ip_configuration {
+#    name                 = "configuration"
+#    subnet_id            = azurerm_subnet.bastion.id
+#    public_ip_address_id = azurerm_public_ip.primary.id
+#  }
+
+#}
+
 # Primary Virtual Network
 resource "azurerm_virtual_network" "primary" {
   name                = "scope-${var.environment}-primary-vnet"
