eb8939f7686342589f192de5039df41045b9cc2c
Chris Dixon <chriswdixon@gmail.com>
2024-10-21 16:51:56 -0500

    reworked.

--- image/client/terraform/azure/DEV/network.tf
+++ image/client/terraform/azure/DEV/network.tf
@@ -7,7 +7,6 @@ provider "azurerm" {
   }
 }
 
-
 ######################
 # Variables
 variable "location" {
@@ -16,71 +15,83 @@ variable "location" {
 variable "environment" {
   default = "DEV"
 }
-
 variable "scope_address_space" {
-  default = ["10.0.0.0/24"]
+  default = ["10.21.0.0/16"]
+}
+variable "scope_frontend_subnet_prefix" {
+  default = ["10.21.0.0/24"]
 }
 
-
-variable "inside_subnet_prefix" {
-  default = "10.0.0.32/27"
+variable "scope_backend_subnet_prefix" {
+  default = ["10.21.1.0/24"]
 }
-
 
 # Use the Existing Provided Resource
 data "azurerm_resource_group" "main" {
-  name = "scope-${var.environment}-ResourceGroup-v8"
+  name = "scope-${var.environment}-ResourceGroup-v9"
 }
-
 output "id" {
   value = data.azurerm_resource_group.main.id
 }
 
-
-resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
-  sku                 = "Premium"
-  admin_enabled       = false
-  georeplications {
-    location                = "West US" 
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
+variable "backend_address_pool_name" {
+    default = "scope-BackendPool"
 }
 
-
-# Primary Virtual Network
-resource "azurerm_virtual_network" "primary" {
-  name                = "scope-${var.environment}-primary-vnet"
-  address_space       = var.scope_address_space
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+variable "frontend_port_name" {
+    default = "scope-FrontendPort"
 }
 
+variable "frontend_ip_configuration_name" {
+    default = "scope-AGIPConfig"
+}
+
+variable "http_setting_name" {
+    default = "scope-HTTPsetting"
+}
+
+variable "listener_name" {
+    default = "scope-Listener"
+}
+
+variable "request_routing_rule_name" {
+    default = "scope-RoutingRule"
+}
 
 # Application Security Gateway with WAF v2 for SIPR connection via DoDIN
-resource "azurerm_public_ip" "main" {
-  name                = "scope-${var.environment}-main-pip"
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "scope-${var.environment}-VNet"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  address_space       = var.scope_address_space
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "scope-${var.environment}-AGSubnet"
+  resource_group_name  = data.azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.scope_frontend_subnet_prefix
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "scope-${var.environment}-BackendSubnet"
+  resource_group_name = data.azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.scope_backend_subnet_prefix
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "scope-${var.environment}-AGPublicIPAddress"
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
-# since these variables are re-used - a locals block makes this more maintainable
-locals {
-  backend_address_pool_name      = "${azurerm_virtual_network.primary.name}-beap"
-  frontend_port_name             = "${azurerm_virtual_network.primary.name}-feport"
-  frontend_ip_configuration_name = "${azurerm_virtual_network.primary.name}-feip"
-  http_setting_name              = "${azurerm_virtual_network.primary.name}-be-htst"
-  listener_name                  = "${azurerm_virtual_network.primary.name}-httplstn"
-  request_routing_rule_name      = "${azurerm_virtual_network.primary.name}-rqrt"
-  redirect_configuration_name    = "${azurerm_virtual_network.primary.name}-rdrcfg"
-}
 
-resource "azurerm_application_gateway" "network" {
-  name                = "main-appgateway"
+resource "azurerm_application_gateway" "main" {
+  name                = "scope-${var.environment}-AppGateway"
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
 
@@ -91,123 +102,71 @@ resource "azurerm_application_gateway" "network" {
   }
 
   gateway_ip_configuration {
-    name      = "my-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.inside.id
+    name      = "scope-${var.environment}-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.frontend.id
   }
 
   frontend_port {
-    name = local.frontend_port_name
+    name = var.frontend_port_name
     port = 80
   }
 
   frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.main.id
+    name                 = var.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.pip.id
   }
 
   backend_address_pool {
-    name = local.backend_address_pool_name
+    name = var.backend_address_pool_name
   }
 
   backend_http_settings {
-    name                  = local.http_setting_name
+    name                  = var.http_setting_name
     cookie_based_affinity = "Disabled"
-    path                  = "/path1/"
     port                  = 80
     protocol              = "Http"
     request_timeout       = 60
   }
 
   http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
+    name                           = var.listener_name
+    frontend_ip_configuration_name = var.frontend_ip_configuration_name
+    frontend_port_name             = var.frontend_port_name
     protocol                       = "Http"
   }
 
   request_routing_rule {
-    name                       = local.request_routing_rule_name
-    priority                   = 9
+    name                       = var.request_routing_rule_name
     rule_type                  = "Basic"
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
+    http_listener_name         = var.listener_name
+    backend_address_pool_name  = var.backend_address_pool_name
+    backend_http_settings_name = var.http_setting_name
+    priority                   = 1
   }
-
 }
 
-# Bastion
-
-#resource "azurerm_public_ip" "bastion" {
-#  name                = "scope-${var.environment}-pip"
-#  location            = data.azurerm_resource_group.main.location
-#  resource_group_name = data.azurerm_resource_group.main.name
-#  allocation_method   = "Static"
-#  sku                 = "Standard"
-#}
-
-#resource "azurerm_virtual_network" "bastion" {
-#  name                = "BastionVNet"
-#  address_space       = ["10.0.0.224/27"]
-#  location            = data.azurerm_resource_group.main.location
-#  resource_group_name = data.azurerm_resource_group.main.name
-#}
-
-#resource "azurerm_subnet" "bastion" {
-#  name                 = "AzureBastionSubnet"
-#  resource_group_name  = data.azurerm_resource_group.main.name
-#  virtual_network_name = azurerm_virtual_network.bastion.name
-#  address_prefixes     = ["10.0.0.224/27"]
-#}
-
-
-#resource "azurerm_bastion_host" "bastion" {
-#  name                = "scope-${var.environment}-bastion"
-#  location            = data.azurerm_resource_group.main.location
-#  resource_group_name = data.azurerm_resource_group.main.name
-#  tunneling_enabled = true
-#  sku = "Standard"
-  
-# ip_configuration {
-#    name                 = "configuration"
-#    subnet_id            = azurerm_subnet.bastion.id
-#    public_ip_address_id = azurerm_public_ip.primary.id
-#  }
-
-#}
-
-
-# Inside Subnet - Will be created by Digital Data
-resource "azurerm_subnet" "inside" {
-  name                 = "scope-${var.environment}-inside-subnet"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.primary.name
-  address_prefixes     = [var.inside_subnet_prefix]
-}
-
-# Inside Network Security Group - Will be created by Digital Data
-resource "azurerm_network_security_group" "inside" {
-  name                = "scope-${var.environment}-inside-NSG"
-  location            = data.azurerm_resource_group.main.location
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "nic-${count.index+1}"
   resource_group_name = data.azurerm_resource_group.main.name
-}
+  location            = data.azurerm_resource_group.main.location
 
-# Inside Route Table - Will be created by Digital Data
-resource "azurerm_route_table" "inside" {
-  name                          = "scope-${var.environment}-inside-routetable"
-  location                      = data.azurerm_resource_group.main.location
-  resource_group_name           = data.azurerm_resource_group.main.name
-  #disable_bgp_route_propagation = false
-
-  route {
-    name           = "inside-route"
-    address_prefix = "${var.inside_subnet_prefix}"
-    next_hop_type  = "VnetLocal"
+  ip_configuration {
+    name                          = "nic-ipconfig-${count.index+1}"
+    subnet_id                     = azurerm_subnet.backend.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
-resource "azurerm_subnet_route_table_association" "inside" {
-  subnet_id      = azurerm_subnet.inside.id
-  route_table_id = azurerm_route_table.inside.id
+
+resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "nic-assoc" {
+  count                   = 2
+  network_interface_id    = azurerm_network_interface.nic[count.index].id
+  ip_configuration_name   = "nic-ipconfig-${count.index+1}"
+  backend_address_pool_id = one(azurerm_application_gateway.main.backend_address_pool).id
+}
+
+output "gateway_frontend_ip" {
+  value = "http://${azurerm_public_ip.pip.ip_address}"
 }
 
 
